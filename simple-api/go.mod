0c715218b9ed9818a82ce59fa3994e091c512373
tarunclub <tarunkm__@outlook.com>
2023-06-09 22:19:48 +0530

    simple-api intitial commit

---
+++ simple-api/go.mod
@@ -0,0 +1,5 @@
+module github.com/tarunclub/simple-api
+
+go 1.20
+
+require github.com/gorilla/mux v1.8.0 // indirect
---
+++ simple-api/main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"encoding/json"
+	"math/rand"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// Model for movies
+type Movie struct {
+	MovieId     string    `json:"movie_id"`
+	MovieName   string    `json:"movie_name"`
+	TicketPrice int       `json:"price"`
+	Director    *Director `json:"director"`
+}
+
+type Director struct {
+	Fullname string `json:"fullname"`
+	Website  string `json:"website"`
+}
+
+var movies []Movie
+
+// Middlewares
+func (m *Movie) IsEmpty() bool {
+	return m.MovieName == ""
+}
+
+func main() {
+
+}
+
+// Serve home route
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>Welcome to movies application API</h1>"))
+}
+
+func getAllMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(movies)
+}
+
+func getOneMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	params := mux.Vars(r)
+
+	for _, val := range movies {
+		if val.MovieId == params["id"] {
+			json.NewEncoder(w).Encode(val)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode("Movie not found")
+}
+
+func addOneMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	// validate - empty data
+	if r.Body == nil {
+		json.NewEncoder(w).Encode("Please add some data")
+	}
+
+	// validate - {}
+	var movie Movie
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	if movie.IsEmpty() {
+		json.NewEncoder(w).Encode("Please add some data")
+		return
+	}
+
+	// Generate a random string of length 10.
+	randString := make([]byte, 10)
+	for i := range randString {
+		randString[i] = byte(rand.Intn(256))
+	}
+
+	// Convert the random string to a string.
+	id := string(randString)
+
+	movie.MovieId = id
+
+	movies := append(movies, movie)
+
+	json.NewEncoder(w).Encode(movies)
+}
